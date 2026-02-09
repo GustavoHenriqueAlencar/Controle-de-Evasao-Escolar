c67b77044fe4fd1d730259e9858c7f20c65b5ca3
GustavoHenriqueAlencar <gustavoferreira.alencar@gmail.com>
2026-02-09 20:13:24 -0300

    First Commit

---
+++ src/main/java/com/evasion_control/evasion_control/EvasionControlApplication.java
@@ -0,0 +1,13 @@
+package com.evasion_control.evasion_control;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvasionControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvasionControlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/evasion_control/evasion_control/EvasionControlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.evasion_control.evasion_control;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvasionControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
